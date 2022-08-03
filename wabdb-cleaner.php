8cf09941318ca92372e022f0f9aaac368864bcc2
andre-dane-dev <110545513+andre-dane-dev@users.noreply.github.com>
2022-08-03 18:41:48 +0200

    Update wabdb-cleaner.php

--- wabdb-cleaner.php
+++ wabdb-cleaner.php
@@ -19,7 +19,7 @@
  * WC requires at least: 6.7.0
  * Tested up to: 6.0.1
  *
- * Copyright (C) 2022 AndreDane
+ * Copyright (C) 2022 andre-dane-dev
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
